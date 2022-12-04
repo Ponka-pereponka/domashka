7086a331e0dd220ed48f70dd29351b33bc73487e
Ponka-pereponka <116254502+Ponka-pereponka@users.noreply.github.com>
2022-12-04 12:00:32 +0300

    Update regObr1.php

--- Diplom/regObr1.php
+++ Diplom/regObr1.php
@@ -1,36 +1,27 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-
 $mysqli = mysqli_connect("localhost", "cyozknem_ponka-pereponka", "Rf7328836Rf", "cyozknem_Ponka-pereponka");
-if ($mysqli == false){
-    print("error");
-}else{
-   print("Соединение установлено успешно");
+if ($mysqli == false) {
+  print("error");
+} else {
+ // print("Соединение установлено успешно");
 
-    $name = $_POST["name"];
-    $lastname = $_POST["lastname"];
-    $email = $_POST["email"];
-    $pass = $_POST["pass"];
-    $pass = password_hash("$pass", PASSWORD_DEFAULT);//шифрование пароля
-   
-    $result=$mysqli->query(" SELECT * FROM `ponka-pereponka` WHERE `email`='$email'");
+  $name = $_POST["name"];
+  $lastname = $_POST["lastname"];
+  $email = $_POST["email"];
+  $pass = $_POST["pass"];
+  $email = trim(mb_strtolower($_POST["email"]));
+  $pass = trim($_POST["pass"]);
+  $pass = password_hash("$pass", PASSWORD_DEFAULT);
 
-    if($result->num_rows != 0) {
-        print("Такой пользователь существует");
+  $result = $mysqli->query("SELECT * FROM `ponka-pereponka` WHERE `email`='$email' AND `pass`='$pass'");
+  $result = $mysqli->query("SELECT * FROM `ponka-pereponka` WHERE `email`='$email'");
 
-    }else{
-        $mysqli->query("INSERT INTO `ponka-pereponka`(`name`, `lastname`, `email`, `pass`) VALUES ('$name','$lastname','$email','$pass')"); 
-
-    }
-
-    //var_dump($result->num_rows);
-   
-   
+  if ($result->num_rows != 0) {
+    print("exist");
+  } else {
+    $mysqli->query("INSERT INTO `ponka-pereponka`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+    print("ok");
+  }
 }
-
-
-//echo "Имя: $name<br>
-//Фамилия: $lastname<br>
-//E-mail: $email<br>
-//Пароль: $pass<hr>"
 ?>
